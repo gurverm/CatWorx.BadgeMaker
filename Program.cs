e50114e21467b7e5dca17e8319fbc0996c14678b
Gurveer Madurai <maduraigurveer@gmail.com>
2023-08-29 10:23:26 -0700

    get and print employee methods defined

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CatWorx.BadgeMaker
 {
@@ -6,7 +7,33 @@ namespace CatWorx.BadgeMaker
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Hello, World!");
+        // this is our employee-getting code
+        List<string> employees = GetEmployees();
+        for(int i = 0; i < employees.Count; i++){
+            Console.WriteLine(employees[i]);
+        }
+    }
+
+    static List<string> GetEmployees()
+        {
+            List<string> employees = new List<string>();
+            while (true) {
+                Console.WriteLine("Please enter a name: (leave empty to exit): ");
+                string input = Console.ReadLine() ?? "";
+                if (input == "") {
+                    break;
+                }
+                employees.Add(input);
+            }
+        // This is important!
+        return employees;
+        }
+
+    static void PrintEmployees(List<string> employees){
+        for(int i = 0; i< employees.Count; i++){
+            Console.WriteLine(employees[i]);
+        }
     }
   }
 }
+
